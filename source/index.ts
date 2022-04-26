d16e6d883135ff10d58804cac2491b2f9b4d5e27
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2022-04-26 11:10:51 -0300

    init

---
+++ source/index.ts
@@ -0,0 +1,17 @@
+import {
+  eventInfo,
+  readInfo,
+  setEventDatabase,
+  setReadDatabase,
+  getEventDatabase,
+  getReadDatabase,
+} from './databaseInfo';
+
+export {
+  eventInfo,
+  readInfo,
+  setEventDatabase,
+  setReadDatabase,
+  getEventDatabase,
+  getReadDatabase,
+};
